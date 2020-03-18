c89ddd9d021368facc327fe9c72c76ed4a8d78b2
ludvi <ludvi@DESKTOP-6U5E7KR>
2020-03-18 15:07:48 +0100

    Finished all the steps and added answers to questions

--- Uppgift3_Ludvig-Baummann Olsson/Program.cs
+++ Uppgift3_Ludvig-Baummann Olsson/Program.cs
@@ -7,27 +7,46 @@ namespace Uppgift3_Ludvig_Baummann_Olsson
     {
         static void Main(string[] args)
         {
-            //det går ej att komma åt personens privata egenskaper, istället går man igenom de publika getsen och setsen
-            //Person p = new Person();
-            //p.Age = 30;
 
-            //3.2 Arv
-            //F-8: Det är i Bird som vi bör göra ändringar eller lägga till attribut som ska påverka alla typers av birds.
-            //F-9: Om alla djur behöver ett nytt attribut eller någon ändring så bör det läggas in i Animal klassen.
+        //det går ej att komma åt personens privata egenskaper, istället går man igenom de publika getsen och setsen
+        //Person p = new Person();
+        //p.Age = 30;
 
-            //3.3 polymorfism
-            //F-7: Man kan inte lägga in hästar i hund listan för att häst klassen inte ärver från hund klassen.
-            //F-8: Både hundar och hästar ärver från animal, däför kan dem båda finnas i en animal lista.
+        //3.2 Arv
+        //F-8: Det är i Bird som vi bör göra ändringar eller lägga till attribut som ska påverka alla typers av birds.
+        //F-9: Om alla djur behöver ett nytt attribut eller någon ändring så bör det läggas in i Animal klassen.
 
-            //F-11: foreach loopen går igenom varje objekt som finns i listan och kör metoden Stats() på objekten.
-            // Metoden Stats() sätter ihop och returnerar en string text,
-            //baserat på vad för värden objektets properties har.
+        //3.3 polymorfism
+        //F-7: Man kan inte lägga in hästar i hund listan för att häst klassen inte ärver från hund klassen.
+        //F-8: Både hundar och hästar ärver från animal, däför kan dem båda finnas i en animal lista.
 
-            //F-15: Nej, man kommer inte åt metoden i animal.
-            // Det är för att animal klassen inte har tillgång till metoden.
-            // Om det är ett animal objekt som är av typen dog i listan,
-            // så måste man först casta från animal klassen till dog klassen,
-            // då får man tillgång till metoden i objektet.
+        //F-11: foreach loopen går igenom varje objekt som finns i listan och kör metoden Stats() på objekten.
+        // Metoden Stats() sätter ihop och returnerar en string text,
+        //baserat på vad för värden objektets properties har.
+
+        //F-15: Nej, man kommer inte åt metoden i animal.
+        // Det är för att animal klassen inte har tillgång till metoden.
+        // Om det är ett animal objekt som är av typen dog i listan,
+        // så måste man först casta från animal klassen till dog klassen,
+        // då får man tillgång till metoden i objektet.
+
+        //3.4 Polymorfism
+
+        //F-11-12: Jag skulle säga att polymorfism är viktigt att kunna just för att det används väldigt
+        // ofta i dem flesta programmerings APIer eller kodbaser man kommer stötta på.
+        // Det används specifikt för att kunna återanvända gränsnittet som redan är byggt,
+        // fast på olika sätt beroende på kontexten man arbetar i.
+        // Som ett exempel så kan man ha ett objekt med en metod vid namn "virtual void AddStat(var Input)"
+        // som kan återanvändas genom override för samtliga variabel typer som man kan tänka sig är en "Stat".
+        // Vilket gör att man inte behöver göra fler än en metod signatur, som exempel AddStatString(), AddStatInt()...
+        // Genom att återanvända samma metod singatur med hjälp av polymorfism,
+        // så kan man med hjälp av system som visual studios intellisense,
+        // direkt se alla olika typer av input som AddStat() kan ta.
+        // Kortsagt, polymorfism gör koden mer strukturedard och läsbar genom återanvänding av signaturer,
+        // därför är det viktigt att kunna.
+
+        //F-13: En markant skillnad är att ett objekt kan bara ärva fån en abstrakt klass,
+        // men kan däremot ärva från ett flertal olika interfaces.
 
             PersonHandler ph = new PersonHandler();
 
@@ -72,6 +91,20 @@ namespace Uppgift3_Ludvig_Baummann_Olsson
             foreach (var dog in dogs)
             {
                 Console.WriteLine(dog.Stats());
+            }
+
+            List<UserError> errors = new List<UserError>
+            {
+                new NumericInputError(),
+                new TextInputError(),
+                new CharInputError(),
+                new DoubleInputError(),
+                new PersonInputError()
+            };
+
+            foreach (var error in errors)
+            {
+                Console.WriteLine(error.UEMessage());
             }
         }
     }
---
+++ Uppgift3_Ludvig-Baummann Olsson/UserError.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Uppgift3_Ludvig_Baummann_Olsson
+{
+    public abstract class UserError
+    {
+        public abstract string UEMessage();
+    }
+
+    class NumericInputError : UserError
+    {
+        public override string UEMessage()
+        {
+            return "You tried to use a numeric input in a text only field. This is an error!";
+        }
+    }
+
+    class TextInputError : UserError
+    {
+        public override string UEMessage()
+        {
+            return "You tried to use a text input in a numeric only field. This is an error!";
+        }
+    }
+
+    class CharInputError : UserError
+    {
+        public override string UEMessage()
+        {
+            return "Error! Invalid input. This field only accepts a single character.";
+        }
+    }
+
+    class DoubleInputError : UserError
+    {
+        public override string UEMessage()
+        {
+            return "Error! Invalid input. This field only accepts numeric values with decimals.";
+        }
+    }
+
+    class PersonInputError : UserError
+    {
+        public override string UEMessage()
+        {
+            return "Error! Invalid input. This field only accepts an object of type Person";
+        }
+    }
+}
